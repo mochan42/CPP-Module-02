87ddb3160069877b0166ff0a0d7a1a4224dd8b16
Monine Chan <mochan@students.42wolfsburg.de>
2023-03-25 20:39:05 +0100

    ex01 completed

--- ex01/inc/Fixed.hpp
+++ ex01/inc/Fixed.hpp
@@ -6,14 +6,13 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 15:40:25 by moninechan        #+#    #+#             */
-/*   Updated: 2023/03/21 19:19:33 by mochan           ###   ########.fr       */
+/*   Updated: 2023/03/25 18:39:40 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
 #include <iostream>
-#include <cstring>
 
 class	Fixed
 {
@@ -23,6 +22,7 @@ class	Fixed
 		Fixed(const int n);
 		Fixed(const float f);
 		~Fixed(); // Destructor
+
 		Fixed&	operator=(const Fixed& src); // Copy assignment operator called
 		int		getRawBits( void ) const;
 		void	setRawBits( int const raw );
--- ex01/src/Fixed.cpp
+++ ex01/src/Fixed.cpp
@@ -6,16 +6,16 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 15:40:20 by moninechan        #+#    #+#             */
-/*   Updated: 2023/03/21 20:12:08 by mochan           ###   ########.fr       */
+/*   Updated: 2023/03/25 20:37:04 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.h"
 
 Fixed::Fixed()
-	: _fixedPointNumberValue(0)
 {
 	std::cout << GREEN << "Default constructor called" << D << "\n";
+	this->_fixedPointNumberValue = 0;
 }
 
 Fixed::Fixed(const Fixed& src)
@@ -25,15 +25,27 @@ Fixed::Fixed(const Fixed& src)
 }
 
 Fixed::Fixed(const int n)
-	: _fixedPointNumberValue(n << _fractionalBits)
 {
 	std::cout << GREEN << "Int constructor called" << D << "\n";
+	_fixedPointNumberValue = n * (1 << _fractionalBits);
 }
 
 Fixed::Fixed(const float f)
-	: _fixedPointNumberValue(roundf(f * (1 << _fractionalBits)))
 {
 	std::cout << GREEN << "Float constructor called" << D << "\n";
+	int	fractional_part;
+	Fixed::_fixedPointNumberValue = ((int) floor (f)) << Fixed::_fractionalBits; // calculate integer part of f and convert into fixed point
+	
+	if (f > 0)
+		fractional_part = (f - (long) f) * 256; // calculate fractional part (f > 0) and transform into fixed point
+	else if (f < 0)
+		fractional_part = -(f - (long) f) * 256; // calculate fractional part (f < 0) and transform into fixed point
+	else
+		fractional_part = 0;
+	if (fractional_part > 255)
+		Fixed::_fixedPointNumberValue += 0;
+	else
+		Fixed::_fixedPointNumberValue += fractional_part;
 }
 
 Fixed::~Fixed()
@@ -44,7 +56,7 @@ Fixed::~Fixed()
 Fixed&	Fixed::operator=(const Fixed& src)
 {
 	std::cout << BLU << "Copy assignment operator called" << D << "\n";
-	this->setRawBits(src.getRawBits());
+	this->_fixedPointNumberValue = src._fixedPointNumberValue ;
 	return (*this);
 }
 
@@ -61,12 +73,12 @@ void	Fixed::setRawBits( int const raw )
 
 float	Fixed::toFloat( void ) const
 {
-	return (this->_fixedPointNumberValue >> _fractionalBits);
+	return (_fixedPointNumberValue / (float)(1 << _fractionalBits));
 }
 
 int		Fixed::toInt( void ) const
 {
-	return (this->_fixedPointNumberValue / _fractionalBits);
+	return (_fixedPointNumberValue / (1 << _fractionalBits));
 }
 
 std::ostream& operator<<( std::ostream& outputStream, const Fixed& number )
